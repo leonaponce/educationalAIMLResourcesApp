35d1d1543b66b29ff0c8325bbb8e434931c66e0a
Leona Ponce <lpon6dev@gmail.com>
2019-01-23 23:48:34 -0500

    Element issue resolution

--- src/app/resource-roadmap/roadmap-edit/roadmap-edit.component.ts
+++ src/app/resource-roadmap/roadmap-edit/roadmap-edit.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-roadmap-edit',
-  templateUrl: './roadmap-edit.component.html',
+  templateUrl:'./roadmap-edit.component.html',
   styleUrls: ['./roadmap-edit.component.css']
 })
 export class RoadmapEditComponent implements OnInit {
@@ -13,3 +13,6 @@ export class RoadmapEditComponent implements OnInit {
   }
 
 }
+
+//'./roadmap-edit.component.html',
+'./resource-roadmap/roadmap-edit/roadmap-edit.component';
