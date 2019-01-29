98e3212b82997888466956166bb41d8bdabc1c37
Leona Ponce <lpon6dev@gmail.com>
2019-01-29 00:02:37 -0500

    Resource-list Edits

--- src/app/resources/resources-list/resources-list.component.ts
+++ src/app/resources/resources-list/resources-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Resource } from '../resource.model';
+
 @Component({
   selector: 'app-resources-list',
   templateUrl: './resources-list.component.html',
   styleUrls: ['./resources-list.component.css']
 })
 export class ResourcesListComponent implements OnInit {
-
+  resources: Resource[] = [
+    new Resource('A test resource', 'Siraj recommended this resource', 'https://pixabay.com/photo-2228610/')
+  ];
   constructor() { }
 
   ngOnInit() {
