b50266559aa8a441502b20ee83f0360e91e73e96
Leona Ponce <lpon6dev@gmail.com>
2019-02-20 23:57:13 -0500

    Edited Resource-roadmap.component

--- src/app/resource-roadmap/resource-roadmap.component.ts
+++ src/app/resource-roadmap/resource-roadmap.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ConceptualCheckpoints } from '../shared/ConceptualCheckpoints.model';
+
 @Component({
   selector: 'app-resource-roadmap',
   templateUrl: './resource-roadmap.component.html',
   styleUrls: ['./resource-roadmap.component.css']
 })
+
 export class ResourceRoadmapComponent implements OnInit {
+  ConceptualCheckpoints = ConceptualCheckpoints [] = [
+    new ConceptualCheckpoints('Python Basics', 5),
+    new ConceptualCheckpoints('Machine Learning Basics', 5),
+  ]
 
   constructor() { }
 
