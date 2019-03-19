27d53700e11345e4cb9a6851341e04db8f25f1b9
Leona Ponce <lpon6dev@gmail.com>
2019-03-19 18:05:15 -0400

    Debug

--- src/app/resources/resources-list/resources-item/resources-item.component.ts
+++ src/app/resources/resources-list/resources-item/resources-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Resource } from '../../resource.model';
 
 @Component({
   selector: 'app-resources-item',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resources-item.component.css']
 })
 export class ResourcesItemComponent implements OnInit {
-
+  @Input() resource: Resource;
+  
   constructor() { }
 
   ngOnInit() {
