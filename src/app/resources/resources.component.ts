a8a28d214b5e8281722f7a8f90069f67741f9a2f
Leona Ponce <lpon6dev@gmail.com>
2019-03-21 12:14:35 -0400

    Data Binding Edits

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Resource } from './resource.model';
 
 @Component({
   selector: 'app-resources',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resources.component.css']
 })
 export class ResourcesComponent implements OnInit {
-
+  selectedResource: Resource
   constructor() { }
 
   ngOnInit() {
