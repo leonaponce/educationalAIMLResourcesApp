846e795576954b3da0f99ade51b5179a470bc20d
Leona Ponce <lpon6dev@gmail.com>
2019-04-04 22:10:46 -0400

    Resources-List

--- src/app/resources/resources-list/resources-list.component.ts
+++ src/app/resources/resources-list/resources-list.component.ts
@@ -9,7 +9,7 @@ import { Resource } from '../resource.model';
 })
 export class ResourcesListComponent implements OnInit {
   @Output()resourceWasSelected = new EventEmitter<Resource>();
-  resources: Resource[] = []
+  resources: Resource[] = [
     new Resource("Kaggle Python Mini-Course", "Delve into Data Science's most important language", "https://pixabay.com/photo-2228610/"),
     new Resource("Kaggle Machine Learning Mini-Course", "Explore Data Science's hottest field", "https://pixabay.com/photo-2228610/"),
     new Resource("Kaggle Deep Learning Mini-Course", "Impress yourself with your new Tensorflow skills", "https://pixabay.com/photo-2228610/")
