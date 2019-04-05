267da89c8d080c039a570512fea80f9807f22ca6
Leona Ponce <lpon6dev@gmail.com>
2019-04-04 22:19:13 -0400

    ConceptualCheckpoints Model Clean

--- src/app/shared/conceptualcheckpoints.module.ts
+++ src/app/shared/conceptualcheckpoints.module.ts
@@ -1,4 +1,3 @@
 export class ConceptualCheckpoints {
-  
-          constructor (public name: string, public amount: number) {}
-          }
+  constructor (public name: string, public amount: number) {}
+}
