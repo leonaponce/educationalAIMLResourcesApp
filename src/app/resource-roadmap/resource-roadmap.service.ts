7388595ea47606adbdd82842dfbcf1e36f02ac52
Leona Ponce <lpon6dev@gmail.com>
2019-04-07 12:02:35 -0400

    Service additions

--- src/app/resource-roadmap/resource-roadmap.service.ts
+++ src/app/resource-roadmap/resource-roadmap.service.ts
@@ -1,3 +1,13 @@
+import { Resources } from './resources.model';
+
 export class ResourceRoadmapService {
-    
+    private resources: Resource[] = [
+        new Resource("Kaggle Python Mini-Course", "Delve into Data Science's most important language", "https://pixabay.com/photo-2228610/"),
+        new Resource("Kaggle Machine Learning Mini-Course", "Explore Data Science's hottest field", "https://pixabay.com/photo-2228610/"),
+        new Resource("Kaggle Deep Learning Mini-Course", "Impress yourself with your new Tensorflow skills", "https://pixabay.com/photo-2228610/")
+      ];
+
+      getResources() {
+          return this.resources.slice();
+      }
 }
--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Resource } from './resource.model';
+
+import { ResourcesService } from './resources.service';
 
 @Component({
   selector: 'app-resources',
   templateUrl: './resources.component.html',
   styleUrls: ['./resources.component.css']
+  providers: [ResourcesService]
 })
 export class ResourcesComponent implements OnInit {
   selectedResource: Resource;
