c2c7137b45d03ab04a6e604f4117aa17e998b770
Leona Ponce <lpon6dev@gmail.com>
2019-04-09 02:27:17 -0400

    Resource Roadmap Service

--- src/app/resource-roadmap/resource-roadmap.service.ts
+++ src/app/resource-roadmap/resource-roadmap.service.ts
@@ -1,13 +1,9 @@
-import { Resources } from './resource.model';
+import { ConceptualCheckpoints } from '../shared/conceptualcheckpoints.module';
 
 export class ResourceRoadmapService {
-    private resources: Resource[] = [
-        new Resource("Kaggle Python Mini-Course", "Delve into Data Science's most important language", "https://pixabay.com/photo-2228610/"),
-        new Resource("Kaggle Machine Learning Mini-Course", "Explore Data Science's hottest field", "https://pixabay.com/photo-2228610/"),
-        new Resource("Kaggle Deep Learning Mini-Course", "Impress yourself with your new Tensorflow skills", "https://pixabay.com/photo-2228610/")
-      ];
-
-      getResources() {
-          return this.resources.slice();
-      }
+    conceptualcheckpoints: ConceptualCheckpoints[] = [
+      new ConceptualCheckpoints('Python Basics', 2),
+      new ConceptualCheckpoints('Machine Learning Basics', 3),
+    ];
+      
 }
