80e964c0f728741985bcde8c9bed86077f97d3a0
Leona Ponce <lpon6dev@gmail.com>
2019-04-11 15:24:53 -0400

    App Routing Debugging

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/core'
+
+import { ResourcesComponent } from './resources/resources.component';
+import { ResourceRoadmapComponent } from './resource-roadmap/resource-roadmap.component';
+const appRouted: Router = [
+   
+]
+
+export class AppRoutingModule
+
