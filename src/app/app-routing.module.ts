a25dbc275ee4b7fb3e762643aeb11cc986ea01bb
Leona Ponce <lpon6dev@gmail.com>
2019-04-11 15:35:20 -0400

    App Routing Additions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,18 @@ import { Routes, RouterModule } from '@angular/core'
 
 import { ResourcesComponent } from './resources/resources.component';
 import { ResourceRoadmapComponent } from './resource-roadmap/resource-roadmap.component';
-const appRouted: Router = [
-   
-]
 
-export class AppRoutingModule
+const appRoutes: Routes = [
+  { path: "", redirectTo: '/resources' },
+  { path: 'resources', component: ResourcesComponent },
+  { path: 'resource-roadmap', component: ResourceRoadmapComponent },
+];
+
+@NgModule({
+imports: [RouterModule.forRoot(appRoutes)],
+exports: [RouterModule]
+})
+export class AppRoutingModule {
+    
+}
 
