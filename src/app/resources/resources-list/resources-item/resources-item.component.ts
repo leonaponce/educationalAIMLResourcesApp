9af1f0f75fe76571151ec214cc89bb10c4a1226a
Leona Ponce <lpon6dev@gmail.com>
2019-04-12 15:16:35 -0400

    Resources-Item Debug

--- src/app/resources/resources-list/resources-item/resources-item.component.ts
+++ src/app/resources/resources-list/resources-item/resources-item.component.ts
@@ -1,17 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Resource } from '../../resource.model';
-import { ResourceService } from '../../resource.service';
 
 @Component({
   selector: 'app-resources-item',
   templateUrl: './resources-item.component.html',
   styleUrls: ['./resources-item.component.css']
 })
-export class ResourceItemComponent implements OnInit {
+export class ResourcesItemsComponent implements OnInit {
   @Input() resource: Resource;
-  
-  constructor(private resourceService: ResourceService) { }
 
   ngOnInit() {
   }
