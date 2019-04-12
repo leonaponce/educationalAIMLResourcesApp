bfbb684cf449ba63fcedc7bf1c7a6191f4c6439e
Leona Ponce <lpon6dev@gmail.com>
2019-04-12 12:33:26 -0400

    Routing Debug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,19 @@ import { Routes, RouterModule } from '@angular/core'
 
 import { ResourcesComponent } from './resources/resources.component';
 import { ResourceRoadmapComponent } from './resource-roadmap/resource-roadmap.component';
+import { ResourcesStartComponent } from './resources/resources-start/resources-start.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/resources', pathMatch: 'full' },
   { path: 'resources', component: ResourcesComponent, children: [
-    {path: ''}
+    {path: '', component: ResourcesStartComponent}
   ] },
   { path: 'resource-roadmap', component: ResourceRoadmapComponent },
 ];
 
 @NgModule({
-imports: [RouterModule.forRoot(appRoutes)],
-exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 
