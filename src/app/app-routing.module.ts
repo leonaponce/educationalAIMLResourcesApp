74cd39e87d35b86bcec71a24ae0edcdc2fd70538
Leona Ponce <lpon6dev@gmail.com>
2019-04-13 19:05:53 -0400

    App routing edits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core'
-import { Routes, RouterModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
 import { ResourcesComponent } from './resources/resources.component';
 import { ResourceRoadmapComponent } from './resource-roadmap/resource-roadmap.component';
-import { ResourcesStartComponent } from './resources/resources-start/resources-start.component';
+import { ResourceStartComponent } from './resources/resource-start/resource-start.component';
 import { ResourceEditComponent } from './resources/resource-edit/resource-edit.component';
-import { ResourcesDetailsComponent } from './resources/resources-details/resources-details.component';
+import { ResourceDetailComponent } from './resources/resource-detail/resource-detail.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/resources', pathMatch: 'full' },
   { path: 'resources', component: ResourcesComponent, children: [
-    { path: '', component: ResourcesStartComponent},
+    { path: '', component: ResourceStartComponent},
     { path: 'new', component: ResourceEditComponent },
-    { path: ':id', component: ResourcesDetailsComponent},
+    { path: ':id', component: ResourceDetailComponent},
     { path: ':id/edit', component: ResourceEditComponent } 
   ] },
   { path: 'resource-roadmap', component: ResourceRoadmapComponent },
