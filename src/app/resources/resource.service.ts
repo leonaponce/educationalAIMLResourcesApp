cce12764fd83e2aeafa669eb3e2353ff8967cc25
Leona Ponce <lpon6dev@gmail.com>
2019-04-14 01:10:30 -0400

    Resource Service Edits

--- src/app/resources/resource.service.ts
+++ src/app/resources/resource.service.ts
@@ -3,6 +3,7 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { Resource } from './resource.model';
 import { ConceptualCheckpoint } from '../shared/conceptualcheckpoint.model';
 import { ResourceRoadmapService } from '../resource-roadmap/resource-roadmap.service';
+import { isContentQueryHost } from '@angular/core/src/render3/util';
 
 @Injectable()
 export class ResourceService {
@@ -12,26 +13,26 @@ export class ResourceService {
         new Resource(
           "Kaggle Python Mini-Course",
           "Delve into Data Science's most important language",
-          "https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F2016%2F05%2FPython-Logo-PNG-Image.png&imgrefurl=http%3A%2F%2Fwww.pngall.com%2Fpython-logo-png&docid=WPCvNLNJeU88wM&tbnid=_3Pyk8wT2qcaVM%3A&vet=10ahUKEwjC2-LrhsLhAhVFnlkKHetMDRMQMwhPKAEwAQ..i&w=360&h=180&bih=390&biw=1800&q=python%20logo&ved=0ahUKEwjC2-LrhsLhAhVFnlkKHetMDRMQMwhPKAEwAQ&iact=mrc&uact=8",
+          //<img src = "assets/img/python-icon.png">,
           [
             new ConceptualCheckpoint ("Python",1),
-            new ConceptualCheckpoint ("Python Applications",1) 
+            new ConceptualCheckpoint ("Python Practice",1) 
           ]),
         new Resource(
           "Kaggle Machine Learning Mini-Course",
           "Explore Data Science's hottest field", 
-          "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.tftus.com%2Fimg%2Fmachine-learning-1.png&imgrefurl=https%3A%2F%2Fwww.tftus.com%2Fmachine-learning.php&docid=CHvjSsTD_XTThM&tbnid=Wes5ZvY0FVaC7M%3A&vet=10ahUKEwj2yfL-hsLhAhUhwFkKHRapAcsQMwhuKBAwEA..i&w=600&h=428&bih=390&biw=1800&q=machine%20learning%20logo&ved=0ahUKEwj2yfL-hsLhAhUhwFkKHRapAcsQMwhuKBAwEA&iact=mrc&uact=8",
+          //<img src = "assets/img/machinelearning.png">,
           [
             new ConceptualCheckpoint ("Machine Learning",1),
-            new ConceptualCheckpoint ("Machine Learning Applications",1) 
+            new ConceptualCheckpoint ("Machine Learning Practice",1) 
           ]),
           new Resource(
            "Kaggle Deep Learning Mini-Course", 
            "Impress yourself with your new Tensorflow skills", 
-           "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Ff0%2Fdb%2Ff5%2Ff0dbf54f437965521e9aa5d6da2cf6c6.png&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F761178774498179725%2F&docid=D2Hql9jdfvZC3M&tbnid=OQqgQaqPcxlB3M%3A&vet=10ahUKEwje4eiMh8LhAhUPtlkKHYKqDXUQMwhXKAEwAQ..i&w=2000&h=1239&bih=390&biw=1800&q=deep%20learning%20logo&ved=0ahUKEwje4eiMh8LhAhUPtlkKHYKqDXUQMwhXKAEwAQ&iact=mrc&uact=8",
+           //<img src = "assets/img/deeplearning.png">,
           [
             new ConceptualCheckpoint ("Tensorflow",1),
-            new ConceptualCheckpoint ("Tensorflow Applications",1) 
+            new ConceptualCheckpoint ("Tensorflow Practice",1) 
           ])
        ];
 
@@ -41,10 +42,11 @@ export class ResourceService {
         return this.resources.slice();
       }
 
-      getResources()
-        return this.resources.slice()[index];
-    
-      addConceptualCheckpointsToResourceRoadmap(conceptualcheckpoints: ConceptualCheckpoint[] ) {
+      getResource(index:number) { 
+        return this.resources[index];
+      }
+
+      addConceptualCheckpointsToResourceRoadmap(conceptualcheckpoints: ConceptualCheckpoint[]) {
          this.slService.addConceptualCheckpoints(conceptualcheckpoints);
       }
 }
