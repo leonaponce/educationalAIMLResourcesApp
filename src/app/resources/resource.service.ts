6d7d2a8b6a4fc7d17f4bcb30af9ce8a408eb2746
Leona Ponce <lpon6dev@gmail.com>
2019-04-14 04:43:18 -0400

    Resources Service

--- src/app/resources/resource.service.ts
+++ src/app/resources/resource.service.ts
@@ -3,7 +3,6 @@ import { EventEmitter, Injectable } from '@angular/core';
 import { Resource } from './resource.model';
 import { ConceptualCheckpoint } from '../shared/conceptualcheckpoint.model';
 import { ResourceRoadmapService } from '../resource-roadmap/resource-roadmap.service';
-import { isContentQueryHost } from '@angular/core/src/render3/util';
 
 @Injectable()
 export class ResourceService {
@@ -12,8 +11,8 @@ export class ResourceService {
     private resources: Resource[] = [
         new Resource(
           "Kaggle Python Mini-Course",
-          "Delve into Data Science's most important language",
-          //<img src = "assets/img/python-icon.png">,
+          "Delve into Data Science's the most important language",
+          "https://freeicons.io/laravel/public/uploads/icons/png/12785093741551942290-128.png",
           [
             new ConceptualCheckpoint ("Python",1),
             new ConceptualCheckpoint ("Python Practice",1) 
@@ -21,7 +20,7 @@ export class ResourceService {
         new Resource(
           "Kaggle Machine Learning Mini-Course",
           "Explore Data Science's hottest field", 
-          //<img src = "assets/img/machinelearning.png">,
+          "https://freeicons.io/laravel/public/uploads/icons/png/18589158161548402157-128.png",
           [
             new ConceptualCheckpoint ("Machine Learning",1),
             new ConceptualCheckpoint ("Machine Learning Practice",1) 
@@ -29,7 +28,7 @@ export class ResourceService {
           new Resource(
            "Kaggle Deep Learning Mini-Course", 
            "Impress yourself with your new Tensorflow skills", 
-           //<img src = "assets/img/deeplearning.png">,
+           "https://freeicons.io/laravel/public/uploads/icons/png/19350075911548402146-64.png",
           [
             new ConceptualCheckpoint ("Tensorflow",1),
             new ConceptualCheckpoint ("Tensorflow Practice",1) 
