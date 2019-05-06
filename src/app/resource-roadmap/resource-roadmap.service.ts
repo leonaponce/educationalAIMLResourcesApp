798f495888529184d53b32cf02e5a445e638d478
Leona Ponce <lpon6dev@gmail.com>
2019-05-06 18:52:58 -0400

    Roadmap Service ts edits

--- src/app/resource-roadmap/resource-roadmap.service.ts
+++ src/app/resource-roadmap/resource-roadmap.service.ts
@@ -1,42 +1,42 @@
 import { ConceptualCheckpoint } from '../shared/conceptualcheckpoint.model';
-import { Subject } from 'rxjs';
+import { Subject } from 'rxjs/Subject';
 
 export class ResourceRoadmapService {
-    conceptualcheckpointsChanged = new Subject<ConceptualCheckpoint[]>();
-    startedEditing = new Subject<number>();
-    private conceptualcheckpoints: ConceptualCheckpoint[] = [
-      new ConceptualCheckpoint('Python Basics', 1),
-      new ConceptualCheckpoint('Machine Learning Basics', 1),
-    ];
-    
-    getConceptualCheckpoints() {
-       return this.conceptualcheckpoints.slice();
-    }
+  conceptualcheckpointsChanged = new Subject<ConceptualCheckpoint[]>();
+  startedEditing = new Subject<number>();
+  private conceptualcheckpoints: ConceptualCheckpoint[] = [
+    new ConceptualCheckpoint('Machine Learning Basics', 5),
+    new ConceptualCheckpoint('Machine Learning Practice', 10),
+  ];
 
-    getConceptualCheckpoint(index: number) {
-      return this.conceptualcheckpoints[index];
-    }
-
-    addConceptualCheckpoint(conceptualcheckpoint: ConceptualCheckpoint) {
-       this.conceptualcheckpoints.push(conceptualcheckpoint);
-       this.conceptualcheckpointsChanged.next(this.conceptualcheckpoints.slice());
-    }
-
-    addConceptualCheckpoints(conceptualcheckpoints: ConceptualCheckpoint[]){
-      // for (let conceptualcheckpoint of conceptualcheckpoints) {
-      //   this.addConceptualCheckpoint(conceptualcheckpoint);
-      // }
-      this.conceptualcheckpoints.push(...conceptualcheckpoints);
-      this.conceptualcheckpointsChanged.next(this.conceptualcheckpoints.slice());
-    }
-
-    updateConceptualCheckpoint(index: number, newConceptualCheckpoint: ConceptualCheckpoint) {
-      this.conceptualcheckpoints[index] = newConceptualCheckpoint;
-      this.conceptualcheckpointsChanged.next(this.conceptualcheckpoints.slice());
-    }
-
-    deleteConceptualCheckpoint(index: number) {
-      this.conceptualcheckpoints.splice(index, 1);
-      this.conceptualcheckpointsChanged.next(this.conceptualcheckpoints.slice());
-    }
+  getConceptualCheckpoints() {
+    return this.conceptualcheckpoints.slice();
   }
+
+  getConceptualCheckpoint(index: number) {
+    return this.conceptualcheckpoints[index];
+  }
+
+  addConceptualCheckpoint(conceptualcheckpoint: ConceptualCheckpoint) {
+    this.conceptualcheckpoints.push(conceptualcheckpoint);
+    this.conceptualcheckpointsChanged.next(this.conceptualcheckpoints.slice());
+  }
+
+  addConceptualCheckpoints(conceptualcheckpoints: ConceptualCheckpoint[]) {
+    // for (let conceptualcheckpoint of conceptualcheckpoints) {
+    //   this.addConceptualCheckpoint(conceptualcheckpoint);
+    // }
+    this.conceptualcheckpoints.push(...conceptualcheckpoints);
+    this.conceptualcheckpointsChanged.next(this.conceptualcheckpoints.slice());
+  }
+
+  updateConceptualCheckpoint(index: number, newConceptualCheckpoint: ConceptualCheckpoint) {
+    this.conceptualcheckpoints[index] = newConceptualCheckpoint;
+    this.conceptualcheckpointsChanged.next(this.conceptualcheckpoints.slice());
+  }
+
+  deleteConceptualCheckpoint(index: number) {
+    this.conceptualcheckpoints.splice(index, 1);
+    this.conceptualcheckpointsChanged.next(this.conceptualcheckpoints.slice());
+  }
+}
